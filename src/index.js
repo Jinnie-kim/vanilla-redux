10f0f6fed2611ccaa5e3bac7520ed405c7b7e9f0
Jinnie-kim <hjk7639@gmail.com>
2022-08-23 21:33:41 +0900

    Todo 추가 로직 구현

--- src/index.js
+++ src/index.js
@@ -7,11 +7,24 @@ const ul = document.querySelector('ul');
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+// 코드 최적화할 수 있는 방법 중 하나
+// action만을 리턴하는 함수
+// 보통 이런 함수는 리듀서 함수 위에 작성한다.
+const addTodo = (text) => {
+  return { type: ADD_TODO, text };
+};
+
+const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
-  console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -21,11 +34,39 @@ const reducer = (state = [], action) => {
 
 const store = legacy_createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text));
+};
+
+const dispatchDeleteTodo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteTodo(id));
+};
+
+const paintTodos = () => {
+  ul.innerHTML = '';
+  const toDos = store.getState();
+  toDos.forEach((toDo) => {
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    button.innerText = 'DEL';
+    button.addEventListener('click', dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintTodos);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddTodo(toDo);
 };
 
 form.addEventListener('submit', onSubmit);
